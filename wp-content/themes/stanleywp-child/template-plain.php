2418822034f1432e7f963b90b73d126843707279
jcaiaffa <jonathan.caiaffa@akendos.com>
2016-08-03 14:33:34 -0500

    Plain template language support

--- wp-content/themes/stanleywp-child/template-plain.php
+++ wp-content/themes/stanleywp-child/template-plain.php
@@ -6,7 +6,13 @@
  */
 ?>
 <?php get_header('plain'); ?>
-
+<?php
+	if ($current_lang == "en") {
+		$back = "Main Site";
+	}else if ($current_lang == "es") {
+		$back = "Sitio Principal";
+	}
+?>
 <div class="container faq">
 	<div class="border">
 		<div class="row">
@@ -14,7 +20,7 @@
 	        	<img src="<?php echo get_stylesheet_directory_uri() ?>/images/big-logo.png" alt="">
 	      	</div>
 			<div class="col-lg-12">
-				<div class="back"><a href="<?php echo get_site_url(); ?>">< All Our Kin Main Site</a> </div>
+				<div class="back"><a href="<?php echo get_site_url(); ?>">< All Our Kin <?php echo($back); ?></a> </div>
 				<img src="<?php echo get_field('header_image', $wp_query->post->ID) ?>" alt="">
 			</div>
 		</div>
