f2a15844ada1373513a77329fbb1552754d3dc73
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-08-17 01:26:00 -0500

    Fixes BH-31

--- wp-content/themes/stanleywp-child/functions.php
+++ wp-content/themes/stanleywp-child/functions.php
@@ -309,4 +309,41 @@ function custom_registration_shortcode() {
 	custom_registration_function();
 	return ob_get_clean();
 }
-?>
+
+
+// Adding custom Styles TinyMCE
+
+// Callback function to insert 'styleselect' into the $buttons array
+function my_mce_buttons_2( $buttons ) {
+	array_unshift( $buttons, 'styleselect' );
+	return $buttons;
+}
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'my_mce_buttons_2' );
+
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {
+	// Define the style_formats array
+	$style_formats = array(
+		// Each array child is a format with it's own settings
+		array(
+			'title' => 'Green Text',
+			'inline' => 'span',
+			'classes' => 'green-text',
+		),
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );
+
+	return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
+//adding editor-style.css to the whole site
+
+function my_mce_editor_style() {
+	wp_enqueue_style( 'style-name', get_stylesheet_directory_uri() .'/editor-style.css');
+}
+add_action( 'wp_enqueue_scripts', 'my_mce_editor_style' );
