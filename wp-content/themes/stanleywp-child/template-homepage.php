bf362fe35926d512528d4072d25395cd34df19bc
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-08-17 02:07:45 -0500

    Started BH-29, no placeholder yet

--- wp-content/themes/stanleywp-child/template-homepage.php
+++ wp-content/themes/stanleywp-child/template-homepage.php
@@ -8,40 +8,7 @@
 <?php get_header(); ?>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/includes/home-functions.js"></script>
 <div class="container">
-	<div class="messagepop pop-r">
-		<div class="row border-bottom">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><h2>Register</h2></div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><a class="close-r" href="/">(close)</a></div>
-		</div>
-		<?php echo do_shortcode('[cr_custom_registration]'); ?>
-	</div>
-	<div class="messagepop pop-l">
-		<div class="row border-bottom">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><h2>Login</h2></div>
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><a class="close-l" href="/">(close)</a></div>
-		</div>
-		<?php
-		$args = array(
-			'echo'           => true,
-			'remember'       => false,
-			'redirect'       => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
-			'form_id'        => 'loginform',
-			'id_username'    => 'user_login',
-			'id_password'    => 'user_pass',
-			'id_remember'    => 'rememberme',
-			'id_submit'      => 'wp-submit',
-			'label_username' => __( 'Username' ),
-			'label_password' => __( 'Password' ),
-			'label_remember' => __( 'Remember Me' ),
-			'label_log_in'   => __( 'Log In' ),
-			'value_username' => '',
-			'value_remember' => false
-		);
-		echo "<div class='register-switch'><a id ='register-click' href='#'>Don't have an account, sign up!</a></div>"; 
-		wp_login_form( $args );
-		echo '<a href="'.wp_lostpassword_url( $redirect ).'">Forgot Password?</a>';
-		?>
-	</div>
+	<?php get_template_part('includes/pop-up-login'); ?>
 	<div class="border">
 		<div class="row">
 			<div class="col-lg-12 nav-head">
