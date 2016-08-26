89d80ca9819d36ca7370cfccfecdc21be628f292
Jonathan Caiaffa <jcdisgraf@hotmail.com>
2016-08-26 13:02:44 -0500

    Fixes BH-38

--- wp-content/themes/stanleywp-child/templates/template-general.php
+++ wp-content/themes/stanleywp-child/templates/template-general.php
@@ -7,6 +7,7 @@
 ?>
 <?php get_header(); ?>
 <div class="container">
+<?php get_template_part('includes/pop-up-login'); ?>
 	<div class="border">
 		<div class="row">
 			<div class="col-lg-12 nav-head">
