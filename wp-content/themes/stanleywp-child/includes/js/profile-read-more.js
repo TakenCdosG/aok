acc4a2e1445812ed6c1ffb0425bd7bc29f55d05d
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-09-05 17:56:15 -0500

    Fixes  BH-59

--- wp-content/themes/stanleywp-child/includes/js/profile-read-more.js
+++ wp-content/themes/stanleywp-child/includes/js/profile-read-more.js
@@ -1,14 +1,29 @@
+function col_same_hight(){
+    var max = 0;
+    jQuery(".profile.p_col").each(function(){
+        jQuery(this).height("");
+    });
+    jQuery(".profile.p_col").each(function(){
+        //console.log(jQuery(this).height());
+        if(jQuery(this).height() > max)
+            max = jQuery(this).height();
+    });
+
+
+
+
+        jQuery(".profile.p_col").height(max);
+
+
+}
+
+
 (function ($) {
   "use strict";
   $(document).ready(function() {
-      var max = 0;
-      $(".profile.p_col").each(function(){
-          if(jQuery(this).height() > max)
-            max = jQuery(this).height();
-      });
 
-      $(".profile.p_col").height(max);
 
+      col_same_hight();
 
       // Configure/customize these variables.
       var showChar = 380; // How many characters are shown by default
@@ -47,4 +62,9 @@
     
   });
 
-})(jQuery); 
+    jQuery( window ).resize(function() {
+        col_same_hight();
+    });
+
+})(jQuery);
+
