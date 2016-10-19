75979eecd73b75e4ff60f0ce988444f7049e7a8c
jcaiaffa <jonathan.caiaffa@akendos.com>
2016-10-19 12:13:08 -0500

    Fixes BH-85

--- wp-content/themes/stanleywp-child/author.php
+++ wp-content/themes/stanleywp-child/author.php
@@ -81,8 +81,10 @@ if (!is_user_logged_in()) {
 			<div class="col-lg-7 col-md-7">
 				<div class="basic-information profile">
 					<h3><?php echo $p_child_care_name; ?></h3>
-                                        <h5><?php echo $p_owner_name; ?></h5>
-					<div class="col-lg-6"><?php echo $p_contact_information; ?></div>
+					<div class="col-lg-6">
+                                            <h5><?php echo $p_owner_name; ?></h5>
+                                            <?php echo $p_contact_information; ?>
+                                        </div>
 					<div class="col-lg-6">
 						<h5>Ages Served</h5>
 						<ul><li><?php echo $label_ages_served; ?></li></ul>
