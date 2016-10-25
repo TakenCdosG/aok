a9eb2cc24abe5c59c6987f8e30a83fa0b4e14b78
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-10-25 16:14:36 -0500

    Remove registration feature and add in new Terms & Conditions form

---
+++ wp-content/themes/stanleywp-child/includes/email-pop-up-login.php
@@ -0,0 +1,13 @@
+<div class="messagepop pop-l">
+    <div class="row border-bottom">
+        <div class="col-md-12 text-right"><a class="close-l" href="/">(close)</a></div>
+    </div>
+    <form method="post" id="emailLoginForm">
+        <div class="form-group">
+            <label for="email">Email *</label>
+            <input name="email" id="email" type="email" class="form-control" placeholder="What is your email?" required>
+        </div>
+        <input type="submit" name="wp-submit" id="wp-submit" class="button-primary ajax-button-email-login-form" value="Subscribe">
+    </form>
+
+</div>
