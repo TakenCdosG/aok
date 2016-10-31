a1997f746891751d05125559e2731008bd254e51
jcaiaffa <jonathan.caiaffa@akendos.com>
2016-10-31 12:38:34 -0500

    Fixes BH-100

--- wp-content/themes/stanleywp-child/templates/template-search-child-care.php
+++ wp-content/themes/stanleywp-child/templates/template-search-child-care.php
@@ -12,6 +12,17 @@
         <div class="row">
             <div class="col-lg-12 nav-head">
                 <?php get_template_part('includes/nav'); ?>
+                <?php $header = array(get_field('first_header_image', $wp_query->post->ID), get_field('second_header_image', $wp_query->post->ID));
+                if(!empty($header[0]) and !empty($header[1])){ ?>
+                <div class="header-image">
+                        <div class="image-one" style="background-image: url(<?php echo $header[0] ?>)"></div>
+                        <div class="image-two" style="background-image: url(<?php echo $header[1] ?>)"></div>
+                </div>
+                <?php } elseif(!empty($header[0])){ ?>
+                <div class="header-image" style="background-image: url(<?php echo $header[0] ?>)"></div>
+                <?php }elseif(!empty($header[1])){ ?>
+                <div class="header-image" style="background-image: url(<?php echo $header[1] ?>)"></div>
+                <?php }?>
             </div>
         </div>
         <div class="row">
