df873ec77c9210034f4a9db39fcfa2042d1958ee
jcaiaffa <jonathan.caiaffa@akendos.com>
2016-11-03 08:46:12 -0500

    Fixes BH-102

--- wp-content/themes/stanleywp-child/templates/template-homepage.php
+++ wp-content/themes/stanleywp-child/templates/template-homepage.php
@@ -47,7 +47,7 @@
                                                                                                                                 
                                                                     <a href="#"><input name="zip_code" class="field zip" type="text" value="<?php echo get_field("homepage_zipcode_text", $wp_query->post->ID); ?>"></a>
                                                                     <div class="clearfix"></div>
-                                                                    <div>*Email Address Required</div>
+                                                                    <div style="color:#fff">*Email Address Required</div>
                                                                 <input name="from_home" value="from_home" hidden>
 
                                                                 <a href="#"><input class="submit" type="submit" value="<?php echo get_field("homepage_search_button", $wp_query->post->ID); ?>"></a>
