d9fd15c6aa232d2d87d42dbdc671228b4f5d9bfc
Maicol Lopez Mora <maicol.lopez@akendos.com>
2016-11-16 16:15:44 -0500

    Fixes BH-112

--- wp-content/themes/stanleywp-child/header.php
+++ wp-content/themes/stanleywp-child/header.php
@@ -33,8 +33,8 @@
 					<div class="logo col-lg-9 col-md-9 col-sm-9">
 						<a href="<?php echo site_url(); ?>">
 							<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/main-logo.png" alt="">
-						</a> 
-						<h2>PROVIDER SHOWCASE</h2>
+							<h2>PROVIDER SHOWCASE</h2>
+						</a>
 					</div>
 					<div class="col-lg-3 col-md-3 col-sm-3">
 						<div class="nav-search">
